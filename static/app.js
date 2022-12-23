803bba89c3a247b82042f5ae3ea623d9e66b4087
Ahmed Elsarta <ahmedelsarta411@gmail.com>
2022-12-23 19:32:26 +0200

    improved Ui elements -Sarta

--- static/app.js
+++ static/app.js
@@ -1,18 +1,18 @@
 //translate section
-let translate_btn = document.getElementById("translate-btn")
-let transX_input = document.getElementById("transX-value")
-let transY_input = document.getElementById("transY-value")
+let action_btn = document.getElementById("action-btn")
+let X_input = document.getElementById("X-value")
+let Y_input = document.getElementById("Y-value")
 let shape = document.getElementById("shape")
-let transX_value = 0
-let transY_value = 0
+let X_value = 0
+let Y_value = 0
 
 
 let angle = 0
-translate_btn.addEventListener("click", translate_shape);
+action_btn.addEventListener("click", translate_shape);
 
 function translate_shape() {
-    transY_value += Number(transY_input.value)
-    transX_value += Number(transX_input.value)
-    console.log(transY_value)
-    shape.style.transform = "translate(" + (transX_value) + "px," + (transY_value) + "px)";
+    Y_value += Number(Y_input.value)
+    X_value += Number(X_input.value)
+    // console.log(transY_value)
+    shape.style.transform = "translate(" + (X_value) + "px," + (Y_value) + "px)";
 }
