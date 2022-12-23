bf8b096a0ca6ad3b4578ab72886c227cb559cba3
Ahmed Elsarta <ahmedelsarta411@gmail.com>
2022-12-23 21:07:35 +0200

    all implemented -Sarta
    all the functions work on their own.
    there is a problem when doing multiple operations in a row, But i'll fix it. I hope

--- static/app.js
+++ static/app.js
@@ -1,18 +1,60 @@
-//translate section
 let action_btn = document.getElementById("action-btn")
 let X_input = document.getElementById("X-value")
 let Y_input = document.getElementById("Y-value")
 let shape = document.getElementById("shape")
-let X_value = 0
-let Y_value = 0
+
+//translate section
+let X_position = 0
+let Y_position = 0 
+let action_list = document.getElementById("action-menu")
 
 
+// scale section 
+let X_scale = 1
+let Y_scale = 1
+
+//rotate section
 let angle = 0
-action_btn.addEventListener("click", translate_shape);
+action_btn.addEventListener("click", action_method);
+// action_list.addEventListener("change", function() {
+//     let action = action_list.value
+//     if(action == "rotate"){
+//         Y_input.disabled=true;
+//     }
+//   });
+
+function action_method() {
+    let action = action_list.value
+    switch(action) {
+          case "translate":
+            translate_shape()
+            break;
+          case "scale":
+            scale_shape()
+            break;
+          case "rotate":
+            rotate_shape()
+            break;
+          default:
+            
+        } 
+}
 
 function translate_shape() {
-    Y_value += Number(Y_input.value)
-    X_value += Number(X_input.value)
-    // console.log(transY_value)
-    shape.style.transform = "translate(" + (X_value) + "px," + (Y_value) + "px)";
+    Y_position += Number(Y_input.value)
+    X_position += Number(X_input.value)
+    console.log(Y_position)
+    shape.style.transform = "translate(" + (X_position) + "px," + (Y_position) + "px)";
+}
+
+function scale_shape() {
+    X_scale *= Number(X_input.value)
+    Y_scale *= Number(Y_input.value)
+    shape.style.transform = "scale(" + (X_scale) + "," + (Y_scale) + ")";
+
+}
+
+function rotate_shape() {
+    angle += Number(X_input.value)
+    shape.style.transform = "rotate(" + (-angle) + "deg)";
 }
