6d86435579f750cefa1d25b9270cb4e318038863
Ahmed Elsarta <ahmedelsarta411@gmail.com>
2022-12-23 17:43:29 +0200

    transformation works -Sarta
    added functions and ui elements for shape transformation

--- static/app.js
+++ static/app.js
@@ -3,12 +3,16 @@ let translate_btn = document.getElementById("translate-btn")
 let transX_input = document.getElementById("transX-value")
 let transY_input = document.getElementById("transY-value")
 let shape = document.getElementById("shape")
+let transX_value = 0
+let transY_value = 0
 
+
+let angle = 0
 translate_btn.addEventListener("click", translate_shape);
 
 function translate_shape() {
-    transY_value = transY_input.value
-    transX_value = transX_input.value
+    transY_value += Number(transY_input.value)
+    transX_value += Number(transX_input.value)
     console.log(transY_value)
-    shape.style.transform = "translate(" + transX_value + "px," + transY_value + "px)";
+    shape.style.transform = "translate(" + (transX_value) + "px," + (transY_value) + "px)";
 }
