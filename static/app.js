e7da0f9ea784f17df1c86df48cac39d2aeb981bd
alaayasser01 <77371903+alaayasser01@users.noreply.github.com>
2022-12-27 21:54:43 +0200

    last changes

--- static/app.js
+++ static/app.js
@@ -3,8 +3,7 @@ let X_input = document.getElementById("X-value")
 let Y_input = document.getElementById("Y-value")
 
 let shape = document.getElementsByClassName("shape")
-// let contain= divElement.getBoundingClientRect()
-console.log(shape)
+
 
 //translate section
 let X_position = 0
@@ -21,25 +20,25 @@ let Y_scale = 1
 
 //rotate section
 let angle = 0
-action_btn.addEventListener("click", action_method);
-// action_list.addEventListener("change", function() {
-//     let action = action_list.value
-//     if(action == "rotate"){
-//         Y_input.disabled=true;
-//     }
-//   });
 
+action_btn.addEventListener("click", action_method);
+let shapes=  document.getElementById("theShape")
+let shape_part=shape[0]
+    
 function action_method() {
     let action = action_list.value
     switch(action) {
           case "translate":
+            Y_input.style.display="block";
             translate_shape()
             break;
           case "scale":
+            Y_input.style.display="block";  
             scale_shape()
             break;
           case "rotate":
-            rotate_shape()
+            Y_input.style.display="none";
+            rotate_shape(shape_part)
             break;
           default:
         } 
@@ -49,8 +48,7 @@ function action_method() {
 function translate_shape() {
     Y_position += Number(Y_input.value)
     X_position += Number(X_input.value)
-    console.log(Y_position)
-    // shape.style.transform = "translate(" + (X_position) + "px," + (Y_position) + "px)";
+    console.log(X_position)
 }
 
 function scale_shape() {
@@ -59,26 +57,29 @@ function scale_shape() {
         Y_scale = Number(Y_input.value)
     }
     
-    // shape.style.transform = "scale(" + (X_scale) + "," + (Y_scale) + ")";
 
 }
 
 function getCenter() {
-              
     divElement = document.querySelector(".container");
-
-    elemRect = divElement.getBoundingClientRect();
-
+    elemRect = divElement.getBoundingClientRect(); 
     elemHeight = elemRect.height;
     elemWidth = elemRect.width;
     x_center= X_position +elemWidth/2;
     y_center= Y_position +elemHeight/2;
     
 }
-
-function rotate_shape() {
-    angle += Number(X_input.value)
-    // shape.style.transform = "rotate(" + (-angle) + "deg)";
+function rotate_shape(shape_part) {
+        getCenter()
+        elemRect =shape_part.getBoundingClientRect();
+        elemHeight = elemRect.height;
+        elemWidth = elemRect.width;
+        angle += Number(X_input.value)
+        xi_center= X_position +elemWidth/2;
+        yi_center= Y_position +elemHeight/2;
+        temp = X_position
+        x_position = (x_center + ((X_position-x_center) * Math.cos(-angle).toFixed(3))) + ((Y_position-y_center) * Math.sin(-angle).toFixed(3));
+        y_position = (y_center - ((temp-x_center) * Math.sin(-angle).toFixed(3))) + ((Y_position-y_center) * Math.cos(-angle).toFixed(3));
 }
 
 function render_shape() {
@@ -87,8 +88,8 @@ function render_shape() {
     transform: matrix(scaleX(), skewY(), skewX(), scaleY(), translateX(), translateY()) rotate(-angle deg)
     */
     for (var i = 0; i < shape.length; i++) {
-        shape[i].style.transform = `matrix(${X_scale}, 0, 0, ${Y_scale}, ${X_position}, ${Y_position}) 
-        rotate(${-angle}deg)`;
-        console.log(shape[i])
+        shape[i].style.transform = `matrix(1, 0, 0, 1, ${X_position}, ${Y_position}) `;
     }
+    shapes.style.transform = `matrix(${X_scale}, 0, 0, ${Y_scale}, 0,0) 
+    rotate(${-angle}deg)`;
 }
